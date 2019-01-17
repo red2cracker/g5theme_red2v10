d4340e6c91d0c1d365a188a16bbaa999b22fc451
leezche <red2cracker@gmail.com>
2019-01-17 13:39:10 +0900

    새글 아이콘 위치 오른쪽으로 수정.

--- skin/latest/red2/latest.skin.php
+++ skin/latest/red2/latest.skin.php
@@ -11,20 +11,16 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
     <?php for ($i=0; $i<count($list); $i++) {  ?>
         <li>
             <?php
-            if ($list[$i]['icon_secret']) echo "<i class=\"fa fa-lock\" aria-hidden=\"true\"></i><span class=\"sound_only\">비밀글</span> ";
 
-            if ($list[$i]['icon_new']) echo "<span class=\"new_icon\">N<span class=\"sound_only\">새글</span></span>";
-
-            if ($list[$i]['icon_hot']) echo "<span class=\"hot_icon\">H<span class=\"sound_only\">인기글</span></span>";
-
- 
             echo "<a href=\"".$list[$i]['href']."\"> ";
             if ($list[$i]['is_notice'])
                 echo "<strong>".$list[$i]['subject']."</strong>";
             else
                 echo $list[$i]['subject'];
 
-
+            if ($list[$i]['icon_secret']) echo " <i class=\"fa fa-lock\" aria-hidden=\"true\"></i><span class=\"sound_only\">비밀글</span> ";
+            if ($list[$i]['icon_new']) echo " <span class=\"new_icon\">N<span class=\"sound_only\">새글</span></span>";
+            if ($list[$i]['icon_hot']) echo " <span class=\"hot_icon\">H<span class=\"sound_only\">인기글</span></span>";
 
             echo "</a>";
 
