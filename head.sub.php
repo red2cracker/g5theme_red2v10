4bcec100b6e91d5651c176e81245ec4ed261e45e
red2cracker <red2cracker@gmail.com>
2019-01-31 16:43:16 +0900

    메일 보내기, 자기소개에서 부트스트랩4 CSS 해제.

--- head.sub.php
+++ head.sub.php
@@ -69,6 +69,10 @@ if($pagename == "memo.php") {
     echo "";
 } else if($pagename == "member_confirm.php") {
     echo "";
+} else if($pagename == "formmail.php") {
+    echo "";
+} else if($pagename == "profile.php") {
+    echo "";
 } else {
     echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">'.PHP_EOL;
 }
